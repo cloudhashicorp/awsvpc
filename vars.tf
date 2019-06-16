ec69841e3b78aed0ffc85e0deb7181bf5d5e2d93
cloudhashicorp <51735965+cloudhashicorp@users.noreply.github.com>
2019-06-17 09:04:01 +1000

    Create vars.tf

---
+++ vars.tf
@@ -0,0 +1,9 @@
+variable "access_key" {
+    description = "Your AWS Access Key"
+    default = "AKIAWDOGXT5QOOB7ZLM3"
+}
+
+variable "secret_key" {
+    description = "Your AWS Secret Key"
+    default = "nMNaB000nULFlQzcIYD9FDqb8+/yeJ4WNJb9Zy73"
+}
